a5667d8ccf64891f0185468d3b9c862c3808b1ff
laban254 <labanrotich6544@gmail.com>
2024-06-21 19:14:32 +0300

    added memcache module

--- webScrapify/settings.py
+++ webScrapify/settings.py
@@ -182,6 +182,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'memcached://127.0.0.1:11211/')
 CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'memcached://127.0.0.1:11211/')
 
+
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
