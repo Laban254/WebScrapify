920823df0f421981a6990af248cbb5c6de028cc0
laban254 <labanrotich6544@gmail.com>
2024-06-25 11:26:41 +0300

    add scheduled scrape metadata

--- webscrapify_app/models.py
+++ webscrapify_app/models.py
@@ -9,3 +9,14 @@ class Notification(models.Model):
 
     def __str__(self):
         return self.message
+
+
+class Metadata(models.Model):
+    schedule_id = models.CharField(max_length=100)  # Assuming schedule ID as a string
+    user_id = models.IntegerField()
+    file_path = models.CharField(max_length=255)  # File path where scraped data is saved
+
+    # Add more fields as needed, like timestamp, etc.
+
+    def __str__(self):
+        return f"Schedule ID: {self.schedule_id}, User ID: {self.user_id}"
